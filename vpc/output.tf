77900ddf3fbca035b37f42f3894e41e031ace12b
Stepan Litsevych <s.litsevych@aimprosoft.com>
2019-12-19 14:03:56 +0200

    updates

--- vpc/output.tf
+++ vpc/output.tf
@@ -1,11 +1,3 @@
-data "aws_ssm_parameter" "vpc_cidr" {
-  name = "/${var.name}/vpc_cidr"
-}
-
-data "aws_ssm_parameter" "db_subnet" {
-  name = "/${var.name}/db-subnet"
-}
-
 output "vpc_id" {
   description = "ID of my VPC"
   value       = aws_vpc.myvpc.id
